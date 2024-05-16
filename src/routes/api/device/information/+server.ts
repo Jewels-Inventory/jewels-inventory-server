c751d88ecd3972a3ef5de3b1814b0045d7ab51d6
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-05-16 11:32:07 +0200

    Code cleanup

--- src/routes/api/device/information/+server.ts
+++ src/routes/api/device/information/+server.ts
@@ -3,28 +3,34 @@ import { getOwnerByToken, updateOwner } from '$lib/database/client';
 import type { Device } from '$lib/database/models';
 
 export async function POST({ request }) {
-	const body = await request.json() as Device;
+	const body = (await request.json()) as Device;
 
 	const authorizationHeader = request.headers.get('Authorization');
 	if (!authorizationHeader) {
-		return json({
-			message: 'Authorization header is missing'
-		}, {
-			status: 401
-		});
+		return json(
+			{
+				message: 'Authorization header is missing'
+			},
+			{
+				status: 401
+			}
+		);
 	}
 
 	const token = authorizationHeader.split(' ')[1];
 	const owner = await getOwnerByToken(token);
 	if (!owner) {
-		return json({
-			message: 'Not authorized'
-		}, {
-			status: 404
-		});
+		return json(
+			{
+				message: 'Not authorized'
+			},
+			{
+				status: 404
+			}
+		);
 	}
 
-	const device = owner.devices.findIndex(d => d.id === body.id);
+	const device = owner.devices.findIndex((d) => d.id === body.id);
 	if (device === -1) {
 		owner.devices.push(body);
 	} else {
